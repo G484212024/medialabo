093448082bc416831eb8c7badddb6b72d65b4cf5
G484212024 <248421@st.takushoku-u.ac.jp>
2025-05-22 18:24:53 +0900

    kazuate

--- kazuate.js
+++ kazuate.js
@@ -1,4 +1,3 @@
-// 答え
 let kotae = Math.floor(Math.random()*10) + 1;
 console.log('答え（デバッグ用）: ' + kotae);
 
@@ -8,31 +7,49 @@ let kaisu = 0;
 // 予想を4回実行する
 // 将来以下の hantei(); の4回の呼び出しを全て削除する
 // 代わりにここでは，ボタンを押したら hantei() を呼び出すイベント処理をする
-hantei();
-hantei();
-hantei();
-hantei();
+let kazuate = document.querySelector('button#check');
+kazuate.addEventListener('click',hantei);
 
 // ボタンを押した後の処理をする関数 hantei() の定義
 function hantei() {
   // 将来ここでは 4 ではなくテキストボックスに指定された数値を yoso に代入する
-  let yoso = 4;
+  let x = document.querySelector('input[name=yoso]');
+  yoso = Number(x.value);
 
+  kaisu=kaisu+1;
+  console.log(kaisu+'回目の予想:'+yoso)
+  let y = document.querySelector('span#kaisu');
+  y.textContent = kaisu;
+
+  let z = document.querySelector('span#answer');
+  z.textContent = yoso;
+
+  let a = document.querySelector('p#result');
+  
   
   // 課題3-1: 正解判定する
   // kotae と yoso が一致するかどうか調べて結果を出力
-    kaisu++;
-    console.log(kaisu+"回目の予想:"+yoso);
-    if(kaisu>3)console.log("答えは "+kotae+" でした．すでにゲームは終わっています");
-    else if(kaisu<=2){
-        if(yoso===kotae)console.log("正解です．おめでとう!");
-            else if(yoso<kotae)console.log("まちがい．答えはもっと大きいですよ");
-            else console.log("まちがい．答えはもっと小さいですよ");
-    }else if(kaisu===3){
-        if(yoso===kotae)console.log("正解です．おめでとう!");
-        else console.log("まちがい．残念でした答えは "+kotae+" です");
-  }
-    
-  
   // 課題3-1における出力先はコンソール
+
+  if(kaisu < 3){
+    if(yoso===kotae){
+        console.log('正解です。おめでとう！')
+        a.textContent = '正解です。おめでとう！'
+      }else if(yoso<kotae){
+        console.log('まちがい。答えはもっと大きいですよ')
+        a.textContent = 'まちがい。答えはもっと大きいですよ'
+    }else if(yoso>kotae){
+        console.log('まちがい。答えはもっと小さいですよ')
+        a.textContent = 'まちがい。答えはもっと小さいですよ'
+    }else{
+        console.log('これが表示されていたら条件分岐ミス')
+        a.textContent = 'これが表示されていたら条件分岐ミス'
+    }
+  }else if(kaisu === 3){
+    console.log('間違い。残念でした答えは'+kotae+'です。')
+    a.textContent = '間違い。残念でした答えは'+kotae+'です。'
+  }else{
+    console.log('答えは'+kotae+'でした。すでにゲームは終わっています')
+    a.textContent = '答えは'+kotae+'でした。すでにゲームは終わっています'
+  }
 }
