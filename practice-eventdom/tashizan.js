f321b9895c5d98d4ec404777665f5d4b37dd7e34
G484212024 <248421@st.takushoku-u.ac.jp>
2025-07-03 00:42:33 +0900

    a

--- practice-eventdom/tashizan.js
+++ practice-eventdom/tashizan.js
@@ -2,11 +2,11 @@ function tashizan(){
 
     let i = document.querySelector('input[name=left]');
     left = Number(i.value);
-    //console.log(i);動作確認
+    console.log(i);
 
     let j = document.querySelector('input[name=right]');
     right = Number(j.value);
-    //console.log(right);動作確認
+    console.log(right);
 
     let answer = left+right;
     console.log('答え：'+answer);
